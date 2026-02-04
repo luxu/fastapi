814d3e5306fc206f449ecf4a730996a16d7ba630
Luciano Martins <luciano.martins@quay.com.br>
2026-02-04 09:49:55 -0300

    aula 1

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,8 +1,16 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+from starlette.responses import HTMLResponse
+
+from fast_zero.schemas import Message
 
 app = FastAPI()
 
 
-@app.get('/')
+@app.get('/',
+         status_code=HTTPStatus.OK,
+         response_model=Message,
+)
 def read_root():
     return {'message': 'Olá mundo'}
