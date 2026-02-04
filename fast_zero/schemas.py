536072ac7c949bd2912cbf05b0c7e3c555dfc150
Luciano Martins <luciano.martins@quay.com.br>
2026-02-04 10:53:20 -0300

    Implementando rotas CRUD

--- fast_zero/schemas.py
+++ fast_zero/schemas.py
@@ -1,4 +1,25 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
+
+
+class UserSchema(BaseModel):
+    username: str
+    email: EmailStr
+    password: str
+
+
+class UserPublic(BaseModel):
+    id: int
+    username: str
+    email: EmailStr
+
+
+class UserList(BaseModel):
+    users: list[UserPublic]
+
 
 class Message(BaseModel):
     message: str
+
+
+class UserDB(UserSchema):
+    id: int
